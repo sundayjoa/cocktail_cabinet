4e60717274d279ec9f9a82295b90a0d881ad7095
sundayjoa <jeonjoo1004@gmail.com>
2024-03-17 13:11:31 +0900

    start

---
+++ cocktail_cabinet/src/main/java/com/example/coctail_cabinet/CocktailCabinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.coctail_cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailCabinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailCabinetApplication.class, args);
+	}
+
+}
---
+++ cocktail_cabinet/src/test/java/com/example/coctail_cabinet/CocktailCabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coctail_cabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailCabinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
